a32bc7e5ce0bae08df932f410e74c8256e2d6b46
OkayJerry <geraldjhoskinsiii@gmail.com>
2022-06-10 17:06:48 -0400

    Reference data structure

--- classes/legend.py
+++ classes/legend.py
@@ -3,51 +3,188 @@ from PyQt5 import QtCore
 from PyQt5 import QtGui
 
 parameters = {
-    "pos": "longitudinally propagating position, [m]",
-    "ref_beta": "speed in the unit of light velocity in vacuum of reference charge state, Lorentz beta, [1]",
-    "ref_bg": "multiplication of beta and gamma of reference charge state, [1]",
-    "ref_gamma": "relativistic energy of reference charge state, Lorentz gamma, [1]",
-    "ref_IonEk": "kinetic energy of reference charge state, [eV/u]",
-    "ref_IonEs": "rest energy of reference charge state, [eV/u]",
-    "ref_IonQ": "macro particle number of reference charge state, [1]",
-    "ref_IonW": "total energy of reference charge state, [eV/u], i.e. W = E_s + E_k",
-    "ref_IonZ": "reference charge to mass ratio, e.g.",
-    "ref_phis": "absolute synchrotron phase of reference charge state, [rad]",
-    "ref_SampleIonK": "wave-vector in cavities with different beta values of reference charge state, [rad]",
-    "ref_Brho": "magnetic rigidity of reference charge state, [Tm]",
-    "Brho": "magnetic rigidity of reference charge state, [Tm]",
-    "xcen": "weight average of all charge states for x, [mm]",
-    "xrms": "general rms beam envelope for x, [mm]",
-    "xpcen": "weight average of all charge states for x', [rad]",
-    "xprms": "general rms beam envelope for x', [rad]",
-    "ycen": "weight average of all charge states for y, [mm]",
-    "yrms": "general rms beam envelope for y, [mm]",
-    "ypcen": "weight average of all charge states for y', [rad]",
-    "yprms": "general rms beam envelope for y', [rad]",
-    "zcen": "weight average of all charge states for phi, [rad]", # phi
-    "zrms": "general rms beam envelope for phi, [rad]", # phi
-    "zpcen": "weight average of all charge states for SE_k, [MeV/u]", # SE_k
-    "zprms": "general rms beam envelope for SE_k, [MeV/u]", # SE_k
-    "xemittance": "weight average of geometrical x emittance, [mm-mrad]",
-    "xnemittance": "weight average of normalized x emittance, [mm-mrad]",
-    "yemittance": "weight average of geometrical y emittance, [mm-mrad]",
-    "ynemittance": "weight average of normalized y emittance, [mm-mrad]",
-    "zemittance": "weight average of geometrical z emittance, [rad-MeV/u]",
-    "znemittance": "weight average of normalized z emittance, [rad-MeV/u]",
-    "xtwiss_beta": "weight average of twiss beta x, [m/rad]",
-    "xtwiss_alpha": "weight average of twiss alpha x, [1]",
-    "xtwiss_gamma": "weight average of twiss gamma x, [rad/m]",
-    "ytwiss_beta": "weight average of twiss beta y, [m/rad]",
-    "ytwiss_alpha": "weight average of twiss alpha y, [1]",
-    "ytwiss_gamma": "weight average of twiss gamma y, [rad/m]",
-    "ztwiss_beta": "weight average of twiss beta z, [rad/MeV/u]",
-    "ztwiss_alpha": "weight average of twiss alpha z, [1]",
-    "ztwiss_gamma": "weight average of twiss gamma z, [MeV/u/rad]",
-    "couple_xy": "weight average of normalized x-y coupling term, [1]",
-    "couple_xpy": "weight average of normalized xp-y coupling term, [1]",
-    "couple_xyp": "weight average of normalized x-yp coupling term, [1]",
-    "couple_xpyp": "weight average of normalized xp-yp coupling term, [1]",
-    "last_caviphi0": "last RF cavity's driven phase, [deg]"
+    "pos": {
+        description: "longitudinally propagating position",
+        unit: "m"
+        },
+    "ref_beta":
+        {
+        description: "speed in the unit of light velocity in vacuum of reference charge state, Lorentz beta",
+        unit: None
+        },
+    "ref_bg":
+        {
+        description: "multiplication of beta and gamma of reference charge state",
+        unit: None
+        },
+    "ref_gamma": {
+        description: "relativistic energy of reference charge state, Lorentz gamma",
+        unit: None
+        },
+    "ref_IonEk": {
+        description: "kinetic energy of reference charge state",
+        unit: "eV/u"
+        },
+    "ref_IonEs": {
+        description: "rest energy of reference charge state",
+        unit: "eV/u"
+        },
+    "ref_IonQ": {
+        description: "macro particle number of reference charge state",
+        unit: None
+        },
+    "ref_IonW": {
+        description: "total energy of reference charge state, i.e. W = E_s + E_k",
+        unit: "eV/u"
+        },
+    "ref_IonZ": {
+        description: "reference charge to mass ratio",
+        unit: None
+        },
+    "ref_phis": {
+        description: "absolute synchrotron phase of reference charge state",
+        unit: "rad"
+        },
+    "ref_SampleIonK": {
+        description: "wave-vector in cavities with different beta values of reference charge state",
+        unit: "rad"
+        },
+    "ref_Brho": {
+        description: "magnetic rigidity of reference charge state",
+        unit: "Tm"
+        },
+    "Brho": {
+        description: "magnetic rigidity of reference charge state",
+        unit: "Tm"
+        },
+    "xcen": {
+        description: "weight average of all charge states for x",
+        unit: "mm"
+        },
+    "xrms": {
+        description: "general rms beam envelope for x]",
+        unit: "mm"
+        },
+    "xpcen": {
+        description: "weight average of all charge states for x'",
+        unit: "rad"
+        },
+    "xprms": {
+        description: "general rms beam envelope for x'",
+        unit: "rad"
+        },
+    "ycen": {
+        description: "weight average of all charge states for y",
+        unit: "mm"
+        },
+    "yrms": {
+        description: "general rms beam envelope for y",
+        unit: "mm"
+        },
+    "ypcen": {
+        description: "weight average of all charge states for y'",
+        unit: "rad"
+        },
+    "yprms": {
+        description: "general rms beam envelope for y'",
+        unit: "rad"
+        },
+    "zcen": {
+        description: "weight average of all charge states for phi",
+        unit: "rad"
+        }, # phi
+    "zrms": {
+        description: "general rms beam envelope for phi",
+        unit: "rad"
+        }, # phi
+    "zpcen": {
+        description: "weight average of all charge states for SE_k",
+        unit: "MeV/u"
+        }, # SE_k
+    "zprms": {
+        description: "general rms beam envelope for SE_k",
+        unit: "MeV/u"
+        }, # SE_k
+    "xemittance": {
+        description: "weight average of geometrical x emittance",
+        unit: "mm-mrad"
+        },
+    "xnemittance": {
+        description: "weight average of normalized x emittance",
+        unit: "mm-mrad"
+        },
+    "yemittance": {
+        description: "weight average of geometrical y emittance",
+        unit: "mm-mrad"
+        },
+    "ynemittance": {
+        description: "weight average of normalized y emittance",
+        unit: "mm-mrad"
+        },
+    "zemittance": {
+        description: "weight average of geometrical z emittance",
+        unit: "rad-MeV/u"
+        },
+    "znemittance": {
+        description: "weight average of normalized z emittance",
+        unit: "rad-MeV/u"
+        },
+    "xtwiss_beta": {
+        description: "weight average of twiss beta x",
+        unit: "m/rad"
+        },
+    "xtwiss_alpha": {
+        description: "weight average of twiss alpha x",
+        unit: None
+        },
+    "xtwiss_gamma": {
+        description: "weight average of twiss gamma x",
+        unit: "rad/m"
+        },
+    "ytwiss_beta": {
+        description: "weight average of twiss beta y",
+        unit: "m/rad"
+        },
+    "ytwiss_alpha": {
+        description: "weight average of twiss alpha y",
+        unit: None
+        },
+    "ytwiss_gamma": {
+        description: "weight average of twiss gamma y",
+        unit: "rad/m"
+        },
+    "ztwiss_beta": {
+        description: "weight average of twiss beta z",
+        unit: "rad/MeV/u"
+        },
+    "ztwiss_alpha": {
+        description: "weight average of twiss alpha z",
+        unit: None
+        },
+    "ztwiss_gamma": {
+        description: "weight average of twiss gamma z",
+        unit: "MeV/u/rad"
+        },
+    "couple_xy": {
+        description: "weight average of normalized x-y coupling term",
+        unit: None
+        },
+    "couple_xpy": {
+        description: "weight average of normalized xp-y coupling term",
+        unit: None
+        },
+    "couple_xyp": {
+        description: "weight average of normalized x-yp coupling term",
+        unit: None
+        },
+    "couple_xpyp": {
+        description: "weight average of normalized xp-yp coupling term",
+        unit: None
+        },
+    "last_caviphi0": {
+        description: "last RF cavity's driven phase",
+        unit: "deg"
+        }
 }
 
 
