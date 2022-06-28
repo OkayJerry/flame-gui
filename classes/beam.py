3822bb5b4be7b5204dce63e624af2454f6920e6d
OkayJerry <geraldjhoskinsiii@gmail.com>
2022-06-28 14:29:54 -0400

    prototype new section

--- classes/beam.py
+++ classes/beam.py
@@ -7,6 +7,35 @@ class BeamStateWindow(QtWidgets.QWidget):
         self.setWindowTitle('Beam State Editor')
         layout = QtWidgets.QGridLayout()
 
+        # universal section
+        self.qa_label = QtWidgets.QLabel()
+        self.energy_label = QtWidgets.QLabel()
+        self.mr_label = QtWidgets.QLabel()
+        self.qa_spin = QtWidgets.QSpinBox()
+        self.energy_spin = QtWidgets.QSpinBox()
+        self.mr_spin = QtWidgets.QSpinBox()
+
+        self.qa_label.setText('Q / A:')
+        self.energy_label.setText('Energy:')
+        self.mr_label.setText('Magnetic Rigidity:')
+        self.qa_spin.setRange(-2147483648, 2147483648)
+        self.energy_spin.setRange(-2147483648, 2147483648)
+        self.mr_spin.setRange(-2147483648, 2147483648)
+        
+        layout.addWidget(self.qa_label, 0, 1)
+        layout.addWidget(self.energy_label, 1, 1)
+        layout.addWidget(self.mr_label, 2, 1)
+        layout.addWidget(self.qa_spin, 0, 2)
+        layout.addWidget(self.energy_spin, 1, 2)
+        layout.addWidget(self.mr_spin, 2, 2)
+
+        # separator
+        h_line = QtWidgets.QFrame()
+        h_line.setFrameShape(QtWidgets.QFrame.HLine)
+        h_line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        layout.addWidget(h_line, 3, 0, 1, 3)
+
+        # variable section
         self.var_box = QtWidgets.QComboBox()
         self.kwrd1_box = QtWidgets.QComboBox()
         self.kwrd2_box = QtWidgets.QComboBox()
@@ -28,19 +57,19 @@ class BeamStateWindow(QtWidgets.QWidget):
             self.kwrd2_box.addItem(kwrd)
 
         self.alpha_spin.setRange(-2147483648, 2147483648)
-        self.kwrd1_spin.setMinimum(0)
-        self.kwrd2_spin.setMinimum(0)
+        self.kwrd1_spin.setRange(0, 2147483648)
+        self.kwrd2_spin.setRange(0, 2147483648)
 
         self.alpha_spin.setValue(0)
 
-        layout.addWidget(self.var_box, 0, 0)
-        layout.addWidget(self.kwrd1_box, 1, 1)
-        layout.addWidget(self.kwrd1_spin, 1, 2)
-        layout.addWidget(self.alpha_label, 2, 1)
-        layout.addWidget(self.alpha_spin, 2, 2)
-        layout.addWidget(self.kwrd2_box, 3, 1)
-        layout.addWidget(self.kwrd2_spin, 3, 2)
-        layout.addWidget(self.commit_button, 4, 2)
+        layout.addWidget(self.var_box, 4, 0)
+        layout.addWidget(self.kwrd1_box, 5, 1)
+        layout.addWidget(self.kwrd1_spin, 5, 2)
+        layout.addWidget(self.alpha_label, 6, 1)
+        layout.addWidget(self.alpha_spin, 6, 2)
+        layout.addWidget(self.kwrd2_box, 7, 1)
+        layout.addWidget(self.kwrd2_spin, 7, 2)
+        layout.addWidget(self.commit_button, 8, 2)
 
         self.setLayout(layout)
 
