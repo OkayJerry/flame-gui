a047ad4dee6e03538f7034971e261a9d312cd553
OkayJerry <geraldjhoskinsiii@gmail.com>
2022-07-22 12:52:25 -0400

    added logo

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sys import argv
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from classes.window import *
 import classes.globals as glb
 
 if __name__ == '__main__':
     window = Window()
+    window.setWindowIcon(QIcon('frib.jpg'))
     window.show()
     glb.app.exec()
